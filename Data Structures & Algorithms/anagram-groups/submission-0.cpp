6f288d29b163bff90acf6d9bb1267bdfc6fdbbd0
DEVSHREE MUNNILAL PATLE <2024bec063@sggs.ac.in>
2026-09-25 13:10:05 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+   vector<vector<string>> groupAnagrams(vector<string>& strs) { // unordered list may 
+	unordered_map<string, vector<string>> mp;
+	vector<vector<string>> ans ;
+	for(string str : strs) {
+		string original = str ;
+		sort ( str.begin() , str.end()) ;
+		mp[str].push_back(original) ;
+	}
+	for(auto it : mp) {
+		ans.push_back(it.second) ;
+	}
+	return ans ;
+}
+};
+    
